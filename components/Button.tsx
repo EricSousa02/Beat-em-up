0707a3abe14b52f33460f74ebaba8b0eddfeece1
EricSousa02 <ericsousadeandrade@gmail.com>
2023-10-19 18:04:59 -0300

    dev

--- components/Button.tsx
+++ components/Button.tsx
@@ -18,7 +18,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
         `
         w-full 
         rounded-full 
-        bg-green-500
+        bg-primary
         border
         border-transparent
         px-3 
--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ const Header: React.FC<HeaderProps> = ({
       className={twMerge(`
         h-fit 
         bg-gradient-to-b 
-        from-emerald-800 
+        from-primary 
         p-6
         `,
         className
